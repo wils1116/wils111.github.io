6fc8ddb6981c6ecf517971580cf9f5c8e816c9bb
LAUREN WILSON <wils1116@msu.edu>
2015-12-11 17:26:36 -0500

    update1

--- main.js
+++ main.js
@@ -7,10 +7,3 @@ animateTheBox();
 var timer = window.setInterval(animateTheBox, 1000);
 
 // Link: http://codepen.io/anon/pen/zrOKXV
-function animateTheBox2() {
-  var randomcolor = '#'+Math.floor(Math.random()*16777215).toString(16);
-  $(".foot").css('background', randomcolor);
-}
-
-animateTheBox2();
-var timer2 = window.setInterval(animateTheBox2, 1000);
